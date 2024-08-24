6149a5100506e7b29057573a40615f9cc7fc906c
Antonio Carrillo <dev.antonio.carrillO@gmail.com>
2024-08-24 12:42:47 -0600

    lualine plugin

---
+++ .config/nvim/lua/tiah/plugins/lualine.lua
@@ -0,0 +1,40 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  name = 'luanine',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function ()
+    local lualine = require('lualine')
+
+    lualine.setup({
+      options = {
+        icons_enabled = true,
+        theme = 'moonfly',
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {
+          {
+            'branch',
+            icon = '󰘬',
+          },
+          'diff',
+          'diagnostics',
+        },
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+      },
+      inactive_sections = {
+        lualine_c = {'filename'},
+      },
+    })
+  end,
+}
