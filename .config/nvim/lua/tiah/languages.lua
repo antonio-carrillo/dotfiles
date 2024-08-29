345b978eded5c89eb491fce2e998b581e98e8176
Antonio Carrillo <dev.antonio.carrillO@gmail.com>
2024-08-29 16:19:32 -0600

    C++ language and format

--- .config/nvim/lua/tiah/languages.lua
+++ .config/nvim/lua/tiah/languages.lua
@@ -1,9 +1,13 @@
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'*'},
-  callback = function (args)
+  pattern = { '*' },
+  callback = function(args)
     local filetype = vim.bo[args.buf].filetype
 
-    if filetype == 'lua' then
+    if filetype == 'cpp' then
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+      vim.opt.shiftwidth = 2
+    elseif filetype == 'lua' then
       vim.opt.tabstop = 2
       vim.opt.softtabstop = 2
       vim.opt.shiftwidth = 2
