3727e32ca17bd45e697a56caf009f4bc1a43fabe
Antonio Carrillo <dev.antonio.carrillO@gmail.com>
2024-08-29 16:21:54 -0600

    navigation and actions improvements

--- .config/zsh/binds.zsh
+++ .config/zsh/binds.zsh
@@ -9,29 +9,29 @@ bindkey '^[b' backward-char
 bindkey '^[f' forward-char
 
 # ctrl + b/f (move one word)
-bindkey '^b' backward-word
-bindkey '^f' forward-word
+bindkey '^b' vi-backward-word
+bindkey '^f' vi-forward-word
 
 # ctrl + alt + b/f (move the whole line)
 bindkey '^[^B' beginning-of-line
 bindkey '^[^F' end-of-line
 
 # ctrl + side arrow (move one word)
-bindkey '^[[1;5D' backward-word
-bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' vi-backward-word
+bindkey '^[[1;5C' vi-forward-word
 
 # home/end (move the whole line)
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
 
 # ctrl + backspace (delete last word)
-bindkey '^H' backward-delete-word
+bindkey '^H' vi-backward-kill-word
 
 # delete (delete next char)
 bindkey '^[[3~' delete-char
 
 # ctrl + delete (delete next word)
-bindkey '^[[3;5~' delete-word
+bindkey '^[[3;5~' kill-word
 
 # alt + enter (insert more shell script lines)
 bindkey '^[^M' self-insert-unmeta
