1451cc8b7dbdb77214a3d0e7cbf60b292101e733
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2024-11-07 17:46:00 -0600

    Telescope for commands and filetypes

--- .config/nvim/lua/tiah/plugins/telescope.lua
+++ .config/nvim/lua/tiah/plugins/telescope.lua
@@ -6,8 +6,9 @@ return {
   },
   config = function()
     local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pc', builtin.commands)
     vim.keymap.set('n', '<leader>pf', builtin.find_files)
-    vim.keymap.set('n', '<leader>pg', builtin.git_files)
+    vim.keymap.set('n', '<leader>pt', builtin.filetypes)
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input('Grep > ') })
     end)
