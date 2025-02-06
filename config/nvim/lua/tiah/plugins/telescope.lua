24942b674fe9beac30758a10cef2f4a2bea6b268
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2025-02-05 18:01:32 -0600

    show buffers keymap

--- config/nvim/lua/tiah/plugins/telescope.lua
+++ config/nvim/lua/tiah/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
     vim.keymap.set('n', '<leader>pc', builtin.commands)
     vim.keymap.set('n', '<leader>pf', builtin.find_files)
     vim.keymap.set('n', '<leader>pt', builtin.filetypes)
+    vim.keymap.set('n', '<leader>pb', builtin.buffers)
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input('Grep > ') })
     end)
