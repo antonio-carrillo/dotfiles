1a49f086ddb0de610c7326c50de68022de87815c
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2025-04-25 17:28:49 -0600

    Cargo env only if it exists

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -11,4 +11,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Rust
-. "$HOME/.cargo/env"
+[ -s "$HOME/.cargo/env" ] && \. "$HOME/.cargo/env"
