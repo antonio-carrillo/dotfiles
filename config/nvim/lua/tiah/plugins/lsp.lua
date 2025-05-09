0e1524d0fe17dd52d7df53965b25c178be35b760
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2025-05-09 00:26:45 -0600

    Mason version to avoid breaking changes

--- config/nvim/lua/tiah/plugins/lsp.lua
+++ config/nvim/lua/tiah/plugins/lsp.lua
@@ -4,8 +4,8 @@ return {
   dependencies = {
     -- LSP Support
     { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    { 'mason-org/mason.nvim', version = '^1.0.0' },
+    { 'mason-org/mason-lspconfig.nvim', version = '^1.0.0' },
 
     -- Autocompletion
     { 'hrsh7th/nvim-cmp' },
