5a64269af4457e01439f032c2278c500e1576e46
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2026-02-04 14:37:01 -0600

    feat(nvim): Lua and Markdown LSP

--- config/nvim/lua/tiah/lsp.lua
+++ config/nvim/lua/tiah/lsp.lua
@@ -13,13 +13,80 @@ vim.lsp.config('bashls', {
 })
 vim.lsp.enable('bashls')
 
+-- HTML
+vim.lsp.config('html_ls', {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  filetypes = { 'html', 'templ' },
+  root_markers = { 'package.json' },
+})
+vim.lsp.enable('html_ls')
+
+-- JSON
+vim.lsp.config('jsonls', {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+})
+vim.lsp.enable('jsonls')
+
 -- Lua
 vim.lsp.config('luals', {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
   root_markers = { '.luarc.json', '.luarc.jsonc' },
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library',
+          -- '${3rd}/busted/library',
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
 })
 vim.lsp.enable('luals')
+
+-- Markdown
+vim.lsp.config('markdown_oxide', {
+  cmd = { 'markdown-oxide' },
+  filetypes = { 'markdown' },
+  root_markers = { '.obsidian', '.moxide.toml' },
+})
+vim.lsp.enable('markdown_oxide')
 
 -- Python
 vim.lsp.config('jedi-language-server', {
@@ -31,7 +98,6 @@ vim.lsp.config('jedi-language-server', {
     'setup.cfg',
     'requirements.txt',
     'Pipfile',
-    '.git',
   },
 })
 vim.lsp.enable('jedi-language-server')
@@ -100,10 +166,8 @@ vim.lsp.config('ts_ls', {
   filetypes = {
     'javascript',
     'javascriptreact',
-    'javascript.jsx',
     'typescript',
     'typescriptreact',
-    'typescript.tsx',
   },
 })
 vim.lsp.enable('ts_ls')
