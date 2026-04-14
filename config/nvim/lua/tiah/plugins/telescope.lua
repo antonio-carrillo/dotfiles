e09d1221dfd66cb2534c1bb15b413e8d4e0d3537
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2026-04-13 18:01:54 -0600

    feat(nvim): telescope update

--- config/nvim/lua/tiah/plugins/telescope.lua
+++ config/nvim/lua/tiah/plugins/telescope.lua
@@ -1,15 +1,17 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
     local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pb', builtin.buffers)
     vim.keymap.set('n', '<leader>pc', builtin.commands)
     vim.keymap.set('n', '<leader>pf', builtin.find_files)
+    vim.keymap.set('n', '<leader>pk', builtin.keymaps)
     vim.keymap.set('n', '<leader>pt', builtin.filetypes)
-    vim.keymap.set('n', '<leader>pb', builtin.buffers)
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input('Grep > ') })
     end)
