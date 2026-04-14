41f894ee2da65aeae5904febacfee04bba969de5
Antonio Carrillo <dev.antonio.carrillo@gmail.com>
2026-04-13 18:03:08 -0600

    feat(nvim): conform config updated

--- config/nvim/lua/tiah/plugins/conform.lua
+++ config/nvim/lua/tiah/plugins/conform.lua
@@ -1,42 +1,41 @@
+-- If you dont have the command, you can install the formatter with Mason.
+
 return {
   'stevearc/conform.nvim',
+  default_format_opts = {
+    async = true,
+    lsp_format = 'fallback',
+  },
   config = function()
-    require('conform').setup({
-      formatters_by_ft ={
+    local conform = require('conform')
+
+    conform.setup({
+      formatters_by_ft = {
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         css = { 'prettierd' },
         javascript = { 'prettierd' },
         javascriptreact = { 'prettierd' },
+        json = { 'jq' },
         lua = { 'stylua' },
+        markdown = { 'mdformat' },
         python = { 'autopep8' },
         typescript = { 'prettierd' },
         typescriptreact = { 'prettierd' },
+        yaml = { 'yamlfmt' },
       },
     })
 
-    vim.api.nvim_create_user_command('Format', function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(
-          0,
-          args.line2 - 1,
-          args.line2,
-          true
-        )[1]
-        range = {
-          start = { args.line1, 0 },
-          ['end'] = { args.line2, end_line:len() },
-        }
-      end
-      require('conform').format({
-        async = true,
-        lsp_format = 'fallback',
-        range = range,
-      })
-    end, { range = true })
+    -- Arguments per formatter
+    conform.formatters.stylua = {
+      append_args = {
+        '--indent-type=spaces',
+        '--indent-width=2',
+        '--quote-style=AutoPreferSingle',
+      },
+    }
 
-    vim.keymap.set('n', '<leader>f', vim.cmd.Format)
-    vim.keymap.set('v', '<leader>f', vim.cmd.Format)
+    -- Keymap
+    vim.keymap.set({ 'n', 'v' }, '<leader>f', conform.format)
   end,
 }
